50660d6d41706d3d41075d02601bfde5facd89ba
yangbys <yangbys@126.com>
2016-05-19 16:43:32 +0800

    c

--- assets/js/totop.js
+++ assets/js/totop.js
@@ -1,29 +1,46 @@
 (function($) {
-    
+
     // When to show the scroll link
-    // higher number = scroll link appears further down the page    
-    var upperLimit = 1000; 
-        
+    // higher number = scroll link appears further down the page
+    var upperLimit = 1000;
+
     // Our scroll link element
     var scrollElem = $('#totop');
-    
+
     // Scroll to top speed
     var scrollSpeed = 500;
-    
-    // Show and hide the scroll to top link based on scroll position    
+
+    // Show and hide the scroll to top link based on scroll position
     scrollElem.hide();
-    $(window).scroll(function () {             
-        var scrollTop = $(document).scrollTop();        
+    $(window).scroll(function () {
+        var scrollTop = $(document).scrollTop();
         if ( scrollTop > upperLimit ) {
-            $(scrollElem).stop().fadeTo(300, 1); // fade back in            
-        }else{        
+            $(scrollElem).stop().fadeTo(300, 1); // fade back in
+        }else{
             $(scrollElem).stop().fadeTo(300, 0); // fade out
         }
     });
 
     // Scroll to top animation on click
-    $(scrollElem).click(function(){ 
-        $('html, body').animate({scrollTop:0}, scrollSpeed); return false; 
+    $(scrollElem).click(function(){
+        $('html, body').animate({scrollTop:0}, scrollSpeed); return false;
+    });
+
+
+    //tags
+    var show_post = function(anchor) {
+        $(".post").hide();
+        var cls = anchor.substr(1);
+        $("." + cls).show();
+        $(".archive-folder").text(cls);
+    }
+    if (document.location.hash) {
+        show_post(document.location.hash);
+    }
+    $('a[href*=#]').click(function() {
+        if (this.hash) {
+            show_post(this.hash);
+        }
     });
 
 })(jQuery);
